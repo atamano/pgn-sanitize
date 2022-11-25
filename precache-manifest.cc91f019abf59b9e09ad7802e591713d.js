128084e1972c96986eb38edcd64b9a054fd94142
atamano <antoine.tamano@gmail.com>
2022-11-26 00:43:18 +0100

    Updates

---
+++ precache-manifest.cc91f019abf59b9e09ad7802e591713d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "9d1c36aa006eaee175d7",
+    "url": "/pgn-sanitize/static/css/main.2a77ed8d.chunk.css"
+  },
+  {
+    "revision": "9d1c36aa006eaee175d7",
+    "url": "/pgn-sanitize/static/js/main.9d1c36aa.chunk.js"
+  },
+  {
+    "revision": "47c6f9c12fc325c47b0f",
+    "url": "/pgn-sanitize/static/js/1.47c6f9c1.chunk.js"
+  },
+  {
+    "revision": "b6493c3727c3e762e70c",
+    "url": "/pgn-sanitize/static/js/runtime~main.b6493c37.js"
+  },
+  {
+    "revision": "a0a15d8bec2fd135c0f011d485ca2223",
+    "url": "/pgn-sanitize/index.html"
+  }
+];
