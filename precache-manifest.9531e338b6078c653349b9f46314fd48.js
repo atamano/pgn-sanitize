bff87c6400c4c578f6ba5a360d326d0cf21e25ef
atamano <antoine.tamano@gmail.com>
2022-11-26 22:54:15 +0100

    Updates

---
+++ precache-manifest.9531e338b6078c653349b9f46314fd48.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c8d47763b9e8b62c945e",
+    "url": "/pgn-sanitize/static/css/main.951cdb46.chunk.css"
+  },
+  {
+    "revision": "c8d47763b9e8b62c945e",
+    "url": "/pgn-sanitize/static/js/main.c8d47763.chunk.js"
+  },
+  {
+    "revision": "e81e0c1f88661eb9a0a1",
+    "url": "/pgn-sanitize/static/js/1.e81e0c1f.chunk.js"
+  },
+  {
+    "revision": "b6493c3727c3e762e70c",
+    "url": "/pgn-sanitize/static/js/runtime~main.b6493c37.js"
+  },
+  {
+    "revision": "9759f09d6eadce4ecbfecf2df2b61270",
+    "url": "/pgn-sanitize/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/pgn-sanitize/precache-manifest.7540bb0bcbc280ee6efd9da8d8804d11.js"
+  "/pgn-sanitize/precache-manifest.9531e338b6078c653349b9f46314fd48.js"
 );
 
 workbox.clientsClaim();
